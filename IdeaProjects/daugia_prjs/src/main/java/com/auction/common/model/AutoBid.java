f7cfe9d5facdb342acbeae7e79e9ed6831444c92
trvtah <vietanhtrinh2509@gmail.com>
2026-04-19 16:15:48 +0700

    Update AutoBid.java

--- IdeaProjects/daugia_prjs/src/main/java/com/auction/common/model/AutoBid.java
+++ IdeaProjects/daugia_prjs/src/main/java/com/auction/common/model/AutoBid.java
@@ -1,23 +1,22 @@
 package com.auction.common.model;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 public class AutoBid {
     private int userId;
     private double maxBid;
     private double increment;
-    private Instant registeredAt; // Dùng để ưu tiên ai đăng ký trước
+    private LocalDateTime registrationTime; // Dùng để ưu tiên ai đăng ký trước
 
-    public AutoBid(int userId, double maxBid, double increment) {
+    public AutoBid(int userId, double maxBid, double increment, LocalDateTime registrationTime) {
         this.userId = userId;
         this.maxBid = maxBid;
         this.increment = increment;
-        this.registeredAt = Instant.now(); 
+        this.registrationTime = registrationTime;
     }
 
-    // Getters
     public int getUserId() { return userId; }
     public double getMaxBid() { return maxBid; }
     public double getIncrement() { return increment; }
-    public Instant getRegisteredAt() { return registeredAt; }
+    public LocalDateTime getRegistrationTime() { return registrationTime; }
 }
